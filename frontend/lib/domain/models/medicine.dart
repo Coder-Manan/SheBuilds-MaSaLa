fe7ccfafe6a90cefb200b866ca702a56f3f487f4
Manan Garg <manantechy@gmail.com>
2023-01-07 16:43:16 +0530

    add image url in medicine model

--- frontend/lib/domain/models/medicine.dart
+++ frontend/lib/domain/models/medicine.dart
@@ -16,20 +16,31 @@ class Medicine {
   @HiveField(3)
   int? price;
 
+  @HiveField(4)
+  String? imgSrc;
+
   Medicine(
       {required this.name,
       required this.desc,
       required this.supplier,
-      required this.price});
+      required this.price,
+      this.imgSrc = "dummy url"});
 
   Medicine.fromJson(Map<String, dynamic> json) {
     name = json['name'];
     desc = json['desc'];
     supplier = json['supplier'];
     price = json['price'];
+    imgSrc = json['imgSrc'];
   }
 
   Map<String, dynamic> toJson() {
-    return {'name': name, 'desc': desc, 'supplier': supplier, 'price': price};
+    return {
+      'name': name,
+      'desc': desc,
+      'supplier': supplier,
+      'price': price,
+      'imgSrc': imgSrc
+    };
   }
 }
